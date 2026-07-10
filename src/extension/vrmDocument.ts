964aa8fa225e65590cdb708ccf654ba0c5e1f048
iririn1998 <iririn199810710@gmail.com>
2026-07-11 00:21:52 +0900

    chore: remove src directory

--- src/extension/vrmDocument.ts
+++
@@ -1,9 +0,0 @@
-import * as vscode from "vscode";
-
-export class VrmDocument implements vscode.CustomDocument {
-  constructor(public readonly uri: vscode.Uri) {}
-
-  dispose(): void {
-    // alphaでは保持リソースなし
-  }
-}
--- src/webview/css.d.ts
+++
@@ -1,2 +0,0 @@
-// tsdownがCSSをバンドルするためのside-effect import用宣言
-declare module "*.css";
